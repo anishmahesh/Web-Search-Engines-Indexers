6cdf4e04eb7e119515199a1ea0f75f7e7822267b
sanchitmehta <sanchit.mehta602@gmail.com>
2016-10-26 02:11:38 -0400

    Code was missing in previous commit . recommitting.

--- src/edu/nyu/cs/cs2580/HTMLDocument.java
+++ src/edu/nyu/cs/cs2580/HTMLDocument.java
@@ -4,4 +4,28 @@ package edu.nyu.cs.cs2580;
  * Created by sanchitmehta on 26/10/16.
  */
 public class HTMLDocument {
+    private String bodyText;
+    private String title;
+
+    public HTMLDocument(String bodyText,String title){
+        this.bodyText = bodyText;
+        this.title = title;
+    }
+
+    public String getBodyText(){
+        return  this.bodyText;
+    }
+
+    public void setBodyText(String bodyText){
+        this.bodyText = bodyText;
+    }
+
+    public String getTitle(){
+        return  this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
 }
--- src/edu/nyu/cs/cs2580/HTMLParse.java
+++ src/edu/nyu/cs/cs2580/HTMLParse.java
@@ -1,7 +1,24 @@
 package edu.nyu.cs.cs2580;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by sanchitmehta on 26/10/16.
  */
 public class HTMLParse {
+
+    public HTMLDocument getDocument(File fileName) throws IOException {
+
+        org.jsoup.nodes.Document doc = Jsoup.parse(fileName, "UTF-8", "https://en.wikipedia.org/wiki/Courant_Institute_of_Mathematical_Sciences");
+        Element body = doc.body();
+        System.out.println(doc.title().replaceFirst("- Wikipedia, the free encyclopedia",""));
+        String bodyText = new String(body.text().toLowerCase());
+        String title = doc.title();
+        HTMLDocument htmlDoc = new HTMLDocument(bodyText,title);
+        return htmlDoc;
+    }
 }
