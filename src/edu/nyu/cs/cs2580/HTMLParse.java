4d70c947c9b4c190bc627f9ea153d516df1be912
sanchitmehta <sanchit.mehta602@gmail.com>
2016-10-27 22:33:00 -0400

    Integrated Stemming to HTMLParser

--- src/edu/nyu/cs/cs2580/HTMLParse.java
+++ src/edu/nyu/cs/cs2580/HTMLParse.java
@@ -11,14 +11,27 @@ import java.io.IOException;
  */
 public class HTMLParse {
 
+    HTMLDocument _currentDocument;
+
     public HTMLDocument getDocument(File fileName) throws IOException {
 
         org.jsoup.nodes.Document doc = Jsoup.parse(fileName, "UTF-8", "https://en.wikipedia.org/wiki/Courant_Institute_of_Mathematical_Sciences");
         Element body = doc.body();
         doc.title().replaceFirst("- Wikipedia, the free encyclopedia","");
         String bodyText = new String(body.text().toLowerCase());
+        //bodyText.replaceAll("\\p{P}", " ");
         String title = doc.title();
-        HTMLDocument htmlDoc = new HTMLDocument(bodyText,title);
-        return htmlDoc;
+        _currentDocument = new HTMLDocument(bodyText,title);
+        stemBodyText();
+        return _currentDocument;
     }
+
+    public void stemBodyText(){
+        Stemmer s = new Stemmer();
+        s.add(_currentDocument.getBodyText().toCharArray(), _currentDocument.getBodyText().length());
+        s.stem();
+        _currentDocument.setBodyText(s.toString());
+    }
+
+
 }
