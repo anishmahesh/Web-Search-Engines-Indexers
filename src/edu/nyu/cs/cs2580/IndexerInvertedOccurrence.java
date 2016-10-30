3020fd56b4fb25cc16a3eaf78c6cf4a68472d74f
anishmahesh <anish.v.mahesh@gmail.com>
2016-10-30 07:35:01 -0400

    Implemented Index file splitting and merging

--- src/edu/nyu/cs/cs2580/IndexerInvertedOccurrence.java
+++ src/edu/nyu/cs/cs2580/IndexerInvertedOccurrence.java
@@ -11,6 +11,12 @@ import javafx.geometry.Pos;
  */
 public class IndexerInvertedOccurrence extends Indexer implements Serializable {
 
+  private static int FILE_COUNT_FOR_INDEX_SPLIT = 500;
+
+  private static int TERM_COUNT_FOR_INDEX_SPLIT = 500;
+
+  int indexCount = 0;
+
   // Maps each term to their integer representation
   private Map<String, Integer> _dictionary = new HashMap<String, Integer>();
   // All unique terms appeared in corpus. Offsets are integer representations.
@@ -50,32 +56,190 @@ public class IndexerInvertedOccurrence extends Indexer implements Serializable {
     System.out.println("Construct index from: " + dir);
 
     HTMLParse htmlParse = new HTMLParse();
-    int i = 0;
+    int fileNum = 0;
     for (File fileName : fileNames) {
-      i++;
       HTMLDocument htmlDocument = htmlParse.getDocument(fileName);
       DocumentIndexed doc = new DocumentIndexed(_documents.size());
 
+//      System.out.println(fileName.getName());
       processDocument(htmlDocument.getBodyText(), doc);
 
       doc.setTitle(htmlDocument.getTitle());
       doc.setUrl(htmlDocument.getUrl());
       _documents.add(doc);
       ++_numDocs;
-//      if (i == 10) break;
+
+      if (fileNum == FILE_COUNT_FOR_INDEX_SPLIT) {
+        indexCount++;
+        System.out.println("Constructing partial index number: " + indexCount);
+
+        persistToFile(indexCount);
+        fileNum = 0;
+      }
+
+      fileNum++;
+
     }
+
+//    indexCount = 20;
+
+    System.out.println("Created partial indexes. Now merging them");
+
+    mergeIndex();
+
+    System.out.println("Splitting index file based on number of terms");
+
+    splitIndexFile();
+
     System.out.println(
             "Indexed " + Integer.toString(_numDocs) + " docs with " +
                     Long.toString(_terms.size()) + " terms.");
 
-    String indexFile = _options._indexPrefix + "/corpus.idx";
-    System.out.println("Store index to: " + indexFile);
-    ObjectOutputStream writer =
-            new ObjectOutputStream(new FileOutputStream(indexFile));
-    writer.writeObject(this);
+//    String indexFile = _options._indexPrefix + "/corpus.idx";
+//    System.out.println("Store index to: " + indexFile);
+//    ObjectOutputStream writer =
+//            new ObjectOutputStream(new FileOutputStream(indexFile));
+//    writer.writeObject(this);
+//    writer.close();
+//
+//    System.out.println("test");
+  }
+
+  private void splitIndexFile() throws IOException {
+    String indexFile = _options._indexPrefix + "/corpus.tsv";
+    BufferedReader reader = new BufferedReader(new FileReader(indexFile));
+
+    String partFile = _options._indexPrefix + "/index-part-0.tsv";
+    BufferedWriter writer = new BufferedWriter(new FileWriter(partFile, true));
+
+    int count = -1;
+    int partCount = 0;
+    String line;
+    while((line = reader.readLine()) != null) {
+      count++;
+
+      if (count == TERM_COUNT_FOR_INDEX_SPLIT) {
+        count = 0;
+        partCount++;
+        writer.close();
+        partFile = _options._indexPrefix + "/index-part-" + partCount + ".tsv";
+        writer = new BufferedWriter(new FileWriter(partFile, true));
+        writer.flush();
+      }
+
+      writer.write(line + "\n");
+      line = reader.readLine();
+      writer.write(line + "\n");
+
+
+    }
+
+    reader.close();
+    writer.close();
+    File index = new File(indexFile);
+    index.delete();
+  }
+
+  private void mergeIndex() throws IOException {
+    String indexFile = _options._indexPrefix + "/corpus.tsv";
+    String firstFile = _options._indexPrefix + "/tempIndex1.tsv";
+
+    File index = new File(indexFile);
+    File first = new File(firstFile);
+
+    for (int i = 2; i <= indexCount; i++) {
+      String secondFile = _options._indexPrefix + "/tempIndex" + i + ".tsv";
+
+      File second = new File(secondFile);
+
+      File mergedFile = mergeFiles(first, second);
+
+      first.delete();
+      second.delete();
+      mergedFile.renameTo(index);
+      first = new File(indexFile);
+    }
+
+  }
+
+  private File mergeFiles(File first, File second) throws IOException {
+    String tempFile = _options._indexPrefix + "/temp.tsv";
+
+    File temp = new File(tempFile);
+    BufferedWriter writer = new BufferedWriter(new FileWriter(temp, true));
+
+    BufferedReader firstReader = new BufferedReader(new FileReader(first));
+    BufferedReader secondReader = new BufferedReader(new FileReader(second));
+
+    String lineInFirstFile = firstReader.readLine();
+    String lineInSecondFile = secondReader.readLine();
+
+    while ((lineInFirstFile != null) && (lineInSecondFile != null)) {
+
+      if (Integer.parseInt(lineInFirstFile) < Integer.parseInt(lineInSecondFile)) {
+        writer.write(lineInFirstFile + "\n");
+        lineInFirstFile = firstReader.readLine();
+        writer.write(lineInFirstFile + "\n");
+
+        lineInFirstFile = firstReader.readLine();
+      }
+      else if (Integer.parseInt(lineInSecondFile) > Integer.parseInt(lineInFirstFile)) {
+        writer.write(lineInSecondFile + "\n");
+        lineInSecondFile = secondReader.readLine();
+        writer.write(lineInSecondFile + "\n");
+
+        lineInSecondFile = secondReader.readLine();
+      }
+      else {
+        writer.write(lineInFirstFile + "\n");
+        lineInFirstFile = firstReader.readLine();
+        lineInSecondFile = secondReader.readLine();
+        writer.write(lineInFirstFile + "\t" + lineInSecondFile + "\n");
+
+        lineInFirstFile = firstReader.readLine();
+        lineInSecondFile = secondReader.readLine();
+      }
+
+    }
+
+    while (lineInFirstFile != null) {
+      writer.write(lineInFirstFile + "\n");
+      lineInFirstFile = firstReader.readLine();
+    }
+
+    while (lineInSecondFile != null) {
+      writer.write(lineInSecondFile + "\n");
+      lineInSecondFile = secondReader.readLine();
+    }
+
+    firstReader.close();
+    secondReader.close();
     writer.close();
 
-    System.out.println("test");
+    return temp;
+  }
+
+  private void persistToFile(int index) throws IOException {
+    String indexFile = _options._indexPrefix + "/tempIndex" + index + ".tsv";
+    BufferedWriter writer = new BufferedWriter(new FileWriter(indexFile));
+
+    List<Integer> termIds = new ArrayList<>();
+    termIds.addAll(_postings.keySet());
+    Collections.sort(termIds);
+
+    for (Integer termId: termIds) {
+      writer.write(termId.toString() + "\n");
+
+      Vector<Integer> docOccs = _postings.get(termId);
+      for (int i = 0; i < docOccs.size(); i++) {
+
+        writer.write(docOccs.get(i).toString() + "\t");
+      }
+      writer.write("\n");
+    }
+
+    writer.close();
+    _postings.clear();
   }
 
   private void processDocument(String content, DocumentIndexed doc) {
@@ -110,11 +274,16 @@ public class IndexerInvertedOccurrence extends Indexer implements Serializable {
       int idx;
       if (_dictionary.containsKey(token)) {
         idx = _dictionary.get(token);
-        _postings.get(idx).addAll(termOccurenceMap.get(token));
       } else {
         idx = _terms.size();
         _terms.add(token);
         _dictionary.put(token, idx);
+      }
+
+      if (_postings.containsKey(idx)) {
+        _postings.get(idx).addAll(termOccurenceMap.get(token));
+      }
+      else {
         _postings.put(idx, termOccurenceMap.get(token));
       }
 
